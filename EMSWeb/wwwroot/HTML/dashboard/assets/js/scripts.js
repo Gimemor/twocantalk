9b0cf21572697513fbfa68523673af2bdb94b6c6
Oleg Brezhnev <oleg-423@yandex.ru>
2021-02-21 13:20:03 +0300

    fixes

--- EMSWeb/wwwroot/HTML/dashboard/assets/js/scripts.js
+++ EMSWeb/wwwroot/HTML/dashboard/assets/js/scripts.js
@@ -187,15 +187,3 @@
 
 
 })(jQuery);
-
-function translate(text, fromIndex, toIndex, onSuccess) {
-    const from = languages.find(x => x.id  == fromIndex);
-    const to = languages.find(x => x.id == toIndex);
-    var url = "https://translation.googleapis.com/language/translate/v2?key=AIzaSyCgDWPzfAMHwS_Pt7GntBUeOCWIHptmOd4";
-    url += "&source=" + from.language;
-    url += "&target=" + to.language;
-    url += "&format=text";
-    url += "&q=" + encodeURIComponent(text);
-
-    $.get(url, onSuccess);
-}
